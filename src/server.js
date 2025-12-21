a08a628f57fc9c446b4f57cd6c5f55ce8f69d910
ragielly <ragielly.19@gmail.com>
2025-12-21 12:03:44 -0300

    feat: adiciona rotas GET e POST para users

--- src/server.js
+++ src/server.js
@@ -1,7 +1,37 @@
 import http from 'node:http' // importação modulos internos
 
+// -Criar usuários
+// -Listagem usuários
+// -Edição de usuários
+// -Remoção de usuários
+
+// -HTTP
+// -Método HTTP
+// -URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+//GET => Busca um recurso do back-end
+//POST => Cria um recurso do back-end
+//PUT => Atualiza um recurso no back-end
+//PATCH => Atualizar uma informação específica  de um recurso no back-end 
+//DELETE =>Deletar um recurso do back-end.
+
+
 //criando servidor
 const server = http.createServer((req, res) => { // recebe dois parametros (request , response)
+    
+    const {method,url} = req //desestruturação de objeto
+    //console.log(method,url)//imprime no terminal qual método e qual rota foram chamadas.
+
+    if(method =='GET' && url == '/users'){
+        //Early return
+        return res.end('Listagem de Usuarios')
+    }
+    if(method == 'POST' && url == '/users'){
+        return res.end('Criação de Usuarios')
+    }
+
     return res.end('Hello World') // retornar mensagem
 })
 
