13f4f23b337a97231bc50f47d591dbd31e184649
ragielly <ragielly.19@gmail.com>
2025-12-26 19:25:07 -0300

    Consumindo uma stream completa

--- streams/fake-to-upload-http-stream.js
+++ streams/fake-to-upload-http-stream.js
@@ -6,7 +6,7 @@ class OneToHundredStream extends Readable{// leitura
     _read(){
         const i = this.index++
         setTimeout(()=>{
-            if (i > 100) {
+            if (i > 5) {
                 this.push(null) // finaliza o stream
             } else {
                 const buf = Buffer.from(String(i) + '\n')
@@ -20,4 +20,8 @@ fetch('http://localhost:3334',{ //É uma requisição HTTP usando fetch.
     method:'POST',// simula um envio de informação.// GET não faz sentido
     body: new OneToHundredStream(),
     duplex:'half'//enviar dados enquanto ainda recebe a resposta
+}).then(response =>{
+    return response.text()
+}).then(data => {
+    console.log(data)
 })
--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -1,7 +1,7 @@
 import http from 'node:http'
 import { Transform } from 'node:stream'
 
-class InverseNumberStream extends Transform{ //ler dados de algum ligar escrever dados pra outro lugar
+class InverseNumberStream extends Transform{ //ler dados de algum lugar e escrever dados pra outro lugar
     _transform(chunk, encoding, callback){
         const transformed = Number(chunk.toString()) * -1 // transformando para negativo
         
@@ -13,9 +13,20 @@ class InverseNumberStream extends Transform{ //ler dados de algum ligar escrever
 
 //req=> ReadeblaStream
 //res=> Writable Stream
-const server = http.createServer((req,res)=>{
-    return req
-    .pipe(new InverseNumberStream())
-    .pipe(res)
+const server = http.createServer( async (req,res)=>{
+    const buffers = [] // cria um array de buffers / pedacinhos 
+
+    for await (const chunk of req){// permite percorrer cada pedacinho e 
+        buffers.push(chunk) // adicionar esses pedacinhos no array de buffers
+    }
+
+    const fullStreamContent = Buffer.concat(buffers).toString()
+
+    console.log(fullStreamContent)
+
+    return res.end(fullStreamContent)
+//     return req
+//     .pipe(new InverseNumberStream())
+//     .pipe(res)
 })
 server.listen(3334)
